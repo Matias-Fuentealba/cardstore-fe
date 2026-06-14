7d0ed530718cc7191201ce141aba58827c88b818
Matias_Fuentealba <matias.exequiel.fuentealba@gmail.com>
2026-06-14 16:35:50 -0400

    fix: navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -57,7 +57,7 @@ export function Navbar() {
         <div className="hidden md:flex items-center gap-1">
           <NavLink to="/singles">Singles</NavLink>
           <NavLink to="/search">Catálogo</NavLink>
-          <NavLink to="/deckbuilder">Deckbuilder</NavLink>
+          {/* <NavLink to="/deckbuilder">Deckbuilder</NavLink> */}
         </div>
 
         {/* Right icons */}
